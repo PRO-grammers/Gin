035d23fa5e31704876769ee50a25edc423bee61d
nickfryer21 <fryerne1@gcc.edu>
2014-09-21 18:52:07 -0400

    Create gin.cpp

---
+++ gin.cpp
@@ -0,0 +1,21 @@
+//Gin Rummy
+/*
+  Authors: Nick Fryer, Logan MacKenzie, Zachary Kuligin, Guilherme Pereira
+  Course: COMP 220, Computer Programming II
+  Date: September 25, 2014
+  Purpose: This program contains the driver program for Program 2
+*/
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+  return 0;
+}
