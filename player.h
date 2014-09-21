33c6ecbb9a3dd3d88cdd91fba6a2aff30211bdf6
nickfryer21 <fryerne1@gcc.edu>
2014-09-21 18:47:30 -0400

    Create player.h

---
+++ player.h
@@ -0,0 +1,17 @@
+#ifndef ___PLAYER_H
+#define ___PLAYER_H
+
+class player
+{
+public:
+  void PlayerTurn();
+  void PrintHand();
+  void SelectCard(char ans);
+  void DiscardCard();
+  void FindSets();
+  bool GotGin();
+private:
+
+};
+
+#endif
