c605eb71808daf8b2003247b3033848fbd29706f
gui8787 <guilhermecolucci@hotmail.com>
2014-09-22 20:02:55 -0400

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,62 +1,66 @@
 #include "player.h"
-#include <string>
+#include "stdafx.h"
 
-using namespace std;
-
-void player::PrintHand()
-{
-  
+ostream &operator<<(ostream& cout, player &p){
+	cout << Card::name;
+	return cout;
 }
 
-void player::PlayerTurn()
-{
-  if(GotGin() != true)
-  {
-    cout << "Here are the cards currently in your hand:" << endl;
-    cout << player.PrintHand() << endl;
-    char ans;
-    cout << "Would you like to pick up the " <<  << " from the discard pile, or" << endl;
-    cout << "pick up from the deck? ('P' for the pile, 'D' for the deck)" << endl;
-    cin >> ans;
-    ans = toupper(ans);
-    while((ans != 'P')&&(ans != 'D'))
-    {
-      cout << "Please enter a 'P' for the discard pile or a 'D' for the deck" << endl;
-      cin >> ans;
-      ans = toupper(ans);
-    }
-    player.SelectCard(ans);
-  }
+void PrintHand(player p){
+	for(int i = 0; i < HAND_SIZE; i++){
+		cout << p.hand.at(i) << " ";	//Need to fix this, make it a cout << card
+	}
+
 }
 
-void player::SelectCard(char ans)
+void PlayerTurn(hand h)
 {
-  if(ans =='P')
-  {
-    
-  }
-  if(ans == 'D')
-  {
-    
-  }
+	if(GotGin() != true)
+	{
+		cout << "Here are the cards currently in your hand:" << endl;
+		cout << PrintHand() << endl;
+		char ans;
+		cout << "Would you like to pick up the " <<  << " from the discard pile, or" << endl;
+		cout << "pick up from the deck? ('P' for the pile, 'D' for the deck)" << endl;
+		cin >> ans;
+		ans = toupper(ans);
+		while((ans != 'P')&&(ans != 'D'))
+		{
+			cout << "Please enter a 'P' for the discard pile or a 'D' for the deck" << endl;
+			cin >> ans;
+			ans = toupper(ans);
+		}
+		SelectCard(ans);
+	}
 }
 
-void player::DiscardCard()
+void SelectCard(char ans)
 {
-  char the_value;
-  string the_suit;
-  cout << "What is the value of the card you would like to discard?" << endl;
-  cin.get(the_value);
-  cout << "What is the suit of the card you would like to discard?" << endl;
-  cin >> the_suit;
+	if(ans =='P')
+	{
+
+	}
+	if(ans == 'D')
+	{
+
+	}
 }
 
-void player::FindSets()
-{
-  
+void DiscardCard(){
+	char the_value;
+	string the_suit;
+	cout << "What is the value of the card you would like to discard?" << endl;
+	cin.get(the_value);
+	cout << "What is the suit of the card you would like to discard?" << endl;
+	cin >> the_suit;
 }
 
-bool player::GotGin()
+void FindSets()
 {
-  
+
+}
+
+bool GotGin()
+{
+
 }
