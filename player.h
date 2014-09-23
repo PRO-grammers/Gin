833fdaaf1ca8ac8a37e43f9525c39eb952a593a2
gui8787 <guilhermecolucci@hotmail.com>
2014-09-22 20:00:57 -0400

    Update player.h
    Still needs some functions, just updating.

--- player.h
+++ player.h
@@ -1,17 +1,28 @@
 #ifndef ___PLAYER_H
 #define ___PLAYER_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#include "card.h"
+#include "Deck.h"
+
+const int HAND_SIZE = 10;
+
 class player
 {
 public:
-  void PlayerTurn();
-  void PrintHand();
-  void SelectCard(char ans);
-  void DiscardCard();
-  void FindSets();
-  bool GotGin();
+	friend ostream &operator<<(ostream& cout, player &p);
+	   void PlayerTurn(player p);
+	friend void PrintHand(player p);
+	void SelectCard(char ans);
+	void DiscardCard(player p, Card card);
+	void FindSets();
+	bool GotGin(player p);
 private:
-
+	vector<Card> hand;
 };
 
 #endif
