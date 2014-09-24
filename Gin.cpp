eddf502b9e062bc006e231bc3b91fc54a1402bcc
nickfryer21 <fryerne1@gcc.edu>
2014-09-24 19:27:20 -0400

    Update Gin.cpp

--- Gin.cpp
+++ Gin.cpp
@@ -3,18 +3,10 @@
  
  void Gin::RunGame()
  {
- 	int turn;
  	player[0].PlayerTurn();
  	while(Knock()==false)
  	{
- 		if(player[0].DiscardCard() >= -1)
- 		{
- 			player[1].PlayerTurn();
- 		}
- 		if(player[1].DiscardCard() >= -1)
- 		{
- 			player[0].PlayerTurn();
- 		}
+ 		
  	}
  }
  
