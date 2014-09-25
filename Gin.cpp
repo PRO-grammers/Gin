a909dd04a2efaf68060f9f246557ee303929bae1
nickfryer21 <fryerne1@gcc.edu>
2014-09-25 14:07:01 -0400

    Update Gin.cpp

--- Gin.cpp
+++ Gin.cpp
@@ -1,7 +1,16 @@
- #include "stdafx.h"
- #include "Gin.h"
- #include "Player.h"
+#include "stdafx.h"
+#include "Deck.h"
+#include "Gin.h"
+#include "Player.h"
  
+void Gin::getPlayer1(Player first_player){
+	player1 = first_player;
+}
+
+void Gin::getPlayer2(Player second_player){
+	player2 = second_player;
+}
+
  void Gin::RunGame()
  {
  	player1.PlayerTurn();
