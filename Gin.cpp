60c58d34759ed902c07c6d9923d9514ecc1afe54
Eomer7 <Eomer7@users.noreply.github.com>
2014-09-25 18:37:22 -0400

    Update Gin.cpp

--- Gin.cpp
+++ Gin.cpp
@@ -2,27 +2,31 @@
 #include "Deck.h"
 #include "Gin.h"
 #include "Player.h"
- 
-void Gin::getPlayer1(Player first_player){
-	player1 = first_player;
+
+using namespace std;
+
+Gin::Gin(){
+	
 }
 
-void Gin::getPlayer2(Player second_player){
-	player2 = second_player;
+void Gin::RunGame(){
+	for(int i = 0; player[i%2].WannaKnock(); i++){
+		player[i].PlayerTurn();
+	}
+	SumPoints();
+	return;
 }
 
- void Gin::RunGame()
- {
- 	player1.PlayerTurn();
- 	while(WannaKnock()==false)
- 	{
- 	 player2.PlayerTurn();
- 	 player1.PlayerTurn();
- 	}
- 	return;
- }
- 
- int Gin::SumPoints(vector<Card> tmp)
- {
- 	
- }
+void Gin::SumPoints(){
+	int one = player[0].Sum();
+	int two = player[1].Sum();
+
+	if(one > two){
+		cout << "Player One wins!!" << endl;	//Do something fancy
+	}else if(one < two){
+		cout << "Player Two wins!!" << endl;
+	}else
+		cout << "Players Tie:}   A little boring and probably contrived. " << endl;
+	return;
+}
+
