d1befea5e26848b58a13609731755fd9e5584a37
Eomer7 <Eomer7@users.noreply.github.com>
2014-09-25 18:34:58 -0400

    Update Card.h

--- Card.h
+++ Card.h
@@ -1,3 +1,11 @@
+/*
+  File: Card.h
+  Author:  Dorian P. Yeager
+  Course:  COMP 220, Computer Programming II
+  Date:    30 August 2013
+  Description:  This file provides the definitions of the data types
+                Suit, Value, and Card.
+*/
 #ifndef ___CARD_H
 #define ___CARD_H
 
@@ -7,7 +15,6 @@ using namespace std;
 
 enum Suit {clubs, diamonds, hearts, spades};
 enum Value {ace, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king};
-
 
 class Card {
 public:
