4d744a9cdf150b3f1bef2aeeccc6c1d9345be812
Eomer7 <Eomer7@users.noreply.github.com>
2014-09-25 18:36:12 -0400

    Update Deck.cpp

--- Deck.cpp
+++ Deck.cpp
@@ -7,22 +7,26 @@
 
 #include "stdafx.h"
 #include "Deck.h"
+#include <ctime>
 
+using namespace std;
 // Initializes deck with all the cards.
 // The deck is initialized organized by suit.
-Deck::Deck(void)
-{
+Deck::Deck(void){
+	srand(time(NULL));
 	Card tmp;
 	for(unsigned int suit = 0; suit < 4; suit++){
 		for(unsigned int value = 0; value < 13; value++){
 			deck.push_back(Card(Value(value),Suit(suit)));
 		}
 	}
+	Shuffle();
+	pile.push(deck.back());
+	deck.pop_back();
 }
 
 // Clears the deck.
-Deck::~Deck(void)
-{
+Deck::~Deck(void){
 	deck.clear();
 }
 
@@ -43,7 +47,11 @@ void Deck::Shuffle(){
 
 // Returns the card on the top of the deck
 // And removes that card from the deck.
+// DealCard also checks if the deck is empty and 
+// has it refilled if it is.
 Card Deck::DealCard(){
+	if(deck.empty())
+		ResetDeck();
 	Card tmp = deck.back();
 	deck.pop_back();
 	return tmp;	
@@ -57,6 +65,7 @@ Card Deck::BottomCard(){
 	return tmp;
 }
 
+// Returns the card off the discard pile, without removing it.
 Card Deck::PeekPile(){
 	return pile.top();
 }
