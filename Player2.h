519c80b2e47abf175728b0aef4f46eeeac90a8e6
Eomer7 <Eomer7@users.noreply.github.com>
2014-09-24 20:31:29 -0400

    Create Player2.h

---
+++ Player2.h
@@ -0,0 +1,51 @@
+#ifndef ___PLAYER_H
+#define ___PLAYER_H
+
+#include "Card.h"
+#include "Deck.h"
+
+
+const int HAND_SIZE = 10;
+const int NUM_SETS = 3;
+
+class Player
+{
+public:
+	Player();
+
+	void PlayerTurn();
+	void PrintHand();
+
+	void SelectCard();
+	void GetCard(Card card);
+	void PickUpDiscard();
+	void DiscardCard(Card& card);
+
+
+	bool CanKnock();
+
+	void SortIntoSets();
+	void SortBySuit();
+	void SortByNumber(int start, int end);
+	bool DoesCardFit(Card card);
+
+	void MatchCards();
+	void FindSets(int card, int& setNum);
+	void FindRuns(int card, int& setNum);
+	bool CheckConflicts(vector<Card> tmp);
+	void ReportErrors(vector<Card> tmp, int error[]);
+	void FixErrors(vector<Card> tmp, int error[]);
+
+
+
+private:
+	void Swap(Card& one, Card& two);
+
+	Card hand[HAND_SIZE];
+	int handSize;
+	vector<Card> sets[4];	//4 arrays because at most you can have 3 sets and then the unmatched cards.
+
+	static Deck deck;
+};
+
+#endif
